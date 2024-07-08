e17297a781057021e4d1822f21fc869386fdae30
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 15:40:27 -0300

    se realiza el registro de usuario junto con el hashed del password para encriptarlo

---
+++ src/controllers/authController.js
@@ -0,0 +1,22 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+export const registerUser = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully." });
+  } catch (error) {
+    console.error("Error registering user:", error.message);
+    res.status(500).json({ message: "Failed to register user" });
+  }
+};
