8c7bb74e5b78a428b733d8e089192c87a07b4168
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 18:52:49 -0300

    configuramos el cors para la conexion con el frontend

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from "express";
 import logger from "morgan";
+import cors from "cors";
 import { authRouter } from "./src/routes/userRoute.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(logger("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api", authRouter);
 
