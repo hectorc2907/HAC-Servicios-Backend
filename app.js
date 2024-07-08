63113726758f56c77f5cba0c71de9bf5fd91ab2b
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 15:05:33 -0300

    configuramos la conexion con la base de datos y levantamos la misma

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hola Mundo</h1>");
+});
+
 export default app;
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./src/db/connect.js";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.BACKEND_PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`server running on port:${PORT}`)
-})
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server running on port:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Error al conectar con MongoDB:`, error.message);
+  });
