5791bed133e49f92928c977e99b110d7be843a95
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-08 16:54:17 -0300

    se genera una configuracion incial del loginUser

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -27,3 +27,24 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: "Failed to register user" });
   }
 };
+
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({ message: "Login Succesfully!" });
+  } catch (error) {
+    console.error("Login failed because:", error.message);
+    res.status(500).json({ message: "Failed server" });
+  }
+};
