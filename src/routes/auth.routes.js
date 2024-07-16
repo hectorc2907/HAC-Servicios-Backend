57a11137a6f54840ea524e337c7901548e7e1a45
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 10:23:05 -0300

    se configura la ruta de login con sus validaciones, probamos mediante thenderClient la ruta y funciona correctamente con sus errores y generador de token

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controllers.js";
+import { register, login } from "../controllers/auth.controllers.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
-import { registerSchema } from "../schemas/auth.schema.js";
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
 const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register);
+router.post("/login", validateSchema(loginSchema), login);
 
 export default router;
