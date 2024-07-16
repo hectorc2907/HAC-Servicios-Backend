4d318fb30d4e87b0a1bd409d46640566b7e2e2d8
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-15 23:47:34 -0300

    generamos un validador de los schemas para el manejo de errores

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,14 +1,2 @@
 import { Router } from "express";
-import {
-  registerUser,
-  loginUser,
-  logoutUser,
-  checkAuth,
-} from "../controllers/authController.js";
-
-export const authRouter = Router();
-
-authRouter.post("/register", registerUser);
-authRouter.post("/login", loginUser);
-authRouter.post("/logout", logoutUser);
-authRouter.get("/check-auth", checkAuth);
+import { register } from "../controllers/auth.controllers.js";
