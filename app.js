3178a829bfe1c18e57153ad5e02e9d2a8dfdd1dc
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-15 22:41:41 -0300

    se configura con mayor claridad el archivo app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(express());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 
