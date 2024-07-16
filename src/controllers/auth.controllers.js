32323187bada2fac0a68617ec687336245b4ab62
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 10:59:51 -0300

    generamos un controlador para recuperar el perfil del usuario logueado

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -70,3 +70,20 @@ export const logout = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const profile = async (req, res) => {
+  try {
+    const userFound = await User.findById(req.user.id);
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    return res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
