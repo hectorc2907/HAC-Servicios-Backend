ffcf3da66d949dde382cd9cbc02a3b0d75b5b640
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 11:44:03 -0300

    se configura la ruta de profile junto con el validateToken para su autorizacion con un token, ademas agregamos cookieParser para que se pueda acceder a las mismas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./src/routes/auth.routes.js";
 
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 app.use("/api", authRoutes);
 
