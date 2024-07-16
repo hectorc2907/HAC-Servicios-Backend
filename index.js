5b7e737f3d3694e89325422686055a1792641e55
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-15 22:47:01 -0300

    se configura el archivo app.js para que se maneje con una funcion asincrona

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import app from "./app.js";
 import { connectDB } from "./src/database/connect.js";
 
-const PORT = process.env.BACKEND_PORT;
+const PORT = process.env.BACKEND_PORT || 3000;
 
-connectDB();
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
 
-app.listen(PORT);
-console.log("Server on port", PORT);
+startServer();
