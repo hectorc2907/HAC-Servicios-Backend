23740baf4ee4f0da80b99f70215cbb6e4131c018
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 10:15:57 -0300

    generamos el controlador del login

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -33,3 +33,31 @@ export const register = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userFound = await User.findOne({ email });
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect password" });
+
+    const token = await createAccessToken({ id: userFound._id });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      craetedAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
