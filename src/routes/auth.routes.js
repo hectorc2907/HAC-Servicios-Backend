dfc531d513d7983bc701f3a9c7fbe35ee15a92f5
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 10:49:21 -0300

    se realiza la configuracion de la ruta del logout, devolviendo respuestas favorables

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controllers.js";
+import { register, login, logout } from "../controllers/auth.controllers.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
 
@@ -7,5 +7,6 @@ const router = Router();
 
 router.post("/register", validateSchema(registerSchema), register);
 router.post("/login", validateSchema(loginSchema), login);
+router.post("/logout", logout);
 
 export default router;
