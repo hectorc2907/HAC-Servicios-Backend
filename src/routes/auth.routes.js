ff9c3b13ebf44d0bc87a02966dcda805c134cd8f
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-15 23:48:56 -0300

    se genera la ruta del registro junto con sus esquemas de validacion

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,2 +1,10 @@
 import { Router } from "express";
 import { register } from "../controllers/auth.controllers.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { registerSchema } from "../schemas/auth.schema.js";
+
+const router = Router();
+
+router.post("/register", validateSchema(registerSchema), register);
+
+export default router;
