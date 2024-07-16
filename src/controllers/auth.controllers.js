455e764f09d903d65b68b8b4f71b7d632e3ce9e8
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-15 23:39:36 -0300

    se refactoriza el controlador de registro

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,35 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      const errorMessage =
+        existingUser.username === username
+          ? "The Username is already in use"
+          : "The Email is already in use";
+      return res.status(400).json([errorMessage]);
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ username, email, password: passwordHash });
+    const userSaved = await newUser.save();
+
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie("token", token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createAt: userSaved.createdAt,
+      updateAt: userSaved.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
