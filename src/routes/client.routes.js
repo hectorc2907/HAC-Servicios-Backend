ecf511b77af26419670a5b9e9b36a76761aab35d
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 23:38:42 -0300

    implementamos las validaciones en la ruta para crear clientes

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -7,12 +7,19 @@ import {
   updateClient,
   deleteClient,
 } from "../controllers/client.controllers.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createClientSchema } from "../schemas/client.schema.js";
 
 const router = Router();
 
 router.get("/client", authRequired, getClients);
 router.get("/client/:id", authRequired, getClient);
-router.post("/client", authRequired, createClient);
+router.post(
+  "/client",
+  authRequired,
+  validateSchema(createClientSchema),
+  createClient
+);
 router.delete("/client/:id", authRequired, deleteClient);
 router.put("/client/:id", authRequired, updateClient);
 
