56ff8dfcffad3fa8e40cbee120d47ee0ceb3fe12
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-16 23:10:22 -0300

    se realiza el controlador para obtener un cliente especifico

--- src/controllers/client.controllers.js
+++ src/controllers/client.controllers.js
@@ -8,3 +8,13 @@ export const getClients = async (req, res) => {
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+export const getClient = async (req, res) => {
+  try {
+    const client = await Client.findById(req.params.id).populate("user");
+    if (!client) return res.status(404).json({ message: "Client not found" });
+    res.json(client);
+  } catch (error) {
+    return res.status(404).json({ message: "Client not found" });
+  }
+};
