ba87841e55c3304f075cd9e17870ff86de0a46fe
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-17 21:34:10 -0300

    generamos un modelo basico de los viajes para interactuar con la base de datos

---
+++ src/models/trips.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const tripsSchema = new mongoose.Schema(
+  {
+    income: { type: Number, required: true, trim: true },
+    expenses: { type: Number, required: true, trim: true },
+    balance: { type: Number, required: true, trim: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Trips", tripsSchema);
