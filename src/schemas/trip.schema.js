35183c31ae0dd113c5a369f556552ce3a630741c
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-17 22:14:02 -0300

    cambiamos de nombre los esquemas por conflictos con los controladores y los implementamos en sus rutas correspondientes

--- src/schemas/trip.schema.js
+++ src/schemas/trip.schema.js
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
-export const createTrip = z.object({
+export const createTripSchema = z.object({
   income: z.number({ required_error: "Income is a Number" }),
   expenses: z.number({ required_error: "Income is a Number" }),
   balance: z.number({ required_error: "Income is a Number" }),
 });
 
-export const updateTrip = z.object({
+export const updateTripSchema = z.object({
   income: z.number({ required_error: "Income is a Number" }).optional(),
   expenses: z.number({ required_error: "Income is a Number" }).optional(),
   balance: z.number({ required_error: "Income is a Number" }).optional(),
