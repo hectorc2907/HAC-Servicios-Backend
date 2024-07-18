cc18c38ca881086fda040009be35dc1ec9d6f7f0
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-17 22:59:30 -0300

    creamos las rutas para ventas

---
+++ src/routes/sale.routes.js
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { authRequired } from "../middlewares/validateToken";
+import {
+  getSales,
+  getSale,
+  createSale,
+  updateSale,
+  deleteSale,
+} from "../controllers/sale.controllers.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { createSaleSchema, updateSaleSchema } from "../schemas/sale.schema.js";
+
+const router = Router();
+
+router.get("/sale", authRequired, getSales);
+router.get("/sale/:id", authRequired, getSale);
+router.post(
+  "/sale",
+  authRequired,
+  validateSchema(createSaleSchema),
+  createSale
+);
+router.delete("/sale/:id", authRequired, deleteSale);
+router.put(
+  "/sale/:id",
+  authRequired,
+  validateSchema(updateSaleSchema),
+  updateSale
+);
+
+export default router;
