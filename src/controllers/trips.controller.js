464e6926c7fe494085d45ab0cec45582e0e60d60
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-17 21:39:45 -0300

    generamos el controlador para obtener un viaje por id que este relacionado con el usuario logueado

--- src/controllers/trips.controller.js
+++ src/controllers/trips.controller.js
@@ -8,3 +8,15 @@ export const getTrips = async (req, res) => {
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+export const getTrip = async (req, res) => {
+  try {
+    const trip = await Trips.findById(req.params.id).populate("user");
+    if (!trip) return res.status(404).json({ message: "Trip not found" });
+    if (trip.user._id.toString() !== req.user.id)
+      return res.status(401).json({ message: "Unauthorized" });
+    res.json(trip);
+  } catch (error) {
+    return res.status(404).json({ message: "Trip not found" });
+  }
+};
