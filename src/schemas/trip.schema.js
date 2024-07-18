e9a3ed578df99044c6ea69936c501093132a2c7c
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-17 22:09:03 -0300

    agregamos un esquema de update con los datos opcionales

--- src/schemas/trip.schema.js
+++ src/schemas/trip.schema.js
@@ -5,3 +5,9 @@ export const createTrip = z.object({
   expenses: z.number({ required_error: "Income is a Number" }),
   balance: z.number({ required_error: "Income is a Number" }),
 });
+
+export const updateTrip = z.object({
+  income: z.number({ required_error: "Income is a Number" }).optional(),
+  expenses: z.number({ required_error: "Income is a Number" }).optional(),
+  balance: z.number({ required_error: "Income is a Number" }).optional(),
+});
