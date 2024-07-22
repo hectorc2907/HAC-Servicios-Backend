dfc5348dabe71e08b98673bee90d051fb83d0af4
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-22 14:05:51 -0300

    se realiza la configuracion de cors

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 app.use(cookieParser());
 
