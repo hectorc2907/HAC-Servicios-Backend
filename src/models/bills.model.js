4f7b6299662c74b100bb15a590508055aeda2671
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-21 23:28:47 -0300

    se configuran los controladores para los gastos

--- src/models/bills.model.js
+++ src/models/bills.model.js
@@ -4,6 +4,11 @@ const billsSchema = new mongoose.Schema({
   types: { type: String, required: true },
   value: { type: Number, required: true },
   description: { type: String, required: true },
+  trip: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Trips",
+    required: true,
+  },
 });
 
 export default mongoose.model("Bills", billsSchema);
